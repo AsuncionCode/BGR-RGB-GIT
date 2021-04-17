c103433ac26f544b2c06d56c3eeceb2333eedcf3
BuildTools <alfonso011977@gmail.com>
2021-04-17 13:51:57 -0400

    Primer commit del proyecto

---
+++ color.py
@@ -0,0 +1,38 @@
+import cv2 as cv 
+import numpy as np 
+
+#------------------------------------------------
+
+'''bgr=np.zeros((300,300,3),dtype=np.uint8)
+
+bgr[:,:,:]=(255,0,127)
+
+cv.imshow('BGR',bgr)'''
+
+#------------------------------------------------
+
+'''image = cv.imread('pitogue.jpg')
+CH1B=image[:,:,0]
+CH2G=image[:,:,1]
+CH3R=image[:,:,2]
+cv.imshow('BGR',np.hstack([CH1B,CH2G,CH3R]))
+
+rgb=cv.cvtColor(image,cv.COLOR_BGR2RGB)
+CH1B=rgb[:,:,0]
+CH2G=rgb[:,:,1]
+CH3R=rgb[:,:,2]
+cv.imshow('RGB',np.hstack([CH1B,CH2G,CH3R]))'''
+
+#------------------------------------------------
+
+image=cv.imread('pitogue.jpg')
+
+gris=cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+
+cv.imshow('GRIS',gris)
+
+cv.imshow('Original', image)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
